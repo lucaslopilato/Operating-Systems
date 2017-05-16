600ad5ad4ee06aaa230c26bfbca5d9f01ad419e0
Wesley Pollek <wpollek@umail.ucsb.edu>
2017-05-15 23:26:25 -0700

    Hint codes added for filling out

--- Project 2/code/userprog/addrspace.cc
+++ Project 2/code/userprog/addrspace.cc
@@ -1,9 +1,9 @@
-// addrspace.cc
+// addrspace.cc 
 //  Routines to manage address spaces (executing user programs).
 //
 //  In order to run a user program, you must:
 //
-//  1. link with the -N -T 0 option
+//  1. link with the -N -T 0 option 
 //  2. run coff2noff to convert the object file to Nachos format
 //    (Nachos object code format is essentially just a simpler
 //    version of the UNIX executable object code format)
@@ -12,38 +12,38 @@
 //    don't need to do this last step)
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation
+// All rights reserved.  See copyright.h for copyright notice and limitation 
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
 #include "system.h"
 #include "addrspace.h"
 #include "noff.h"
-
+#include "machine.h" // definition of PageSize
 #ifdef HOST_SPARC
 #include <strings.h>
 #endif
 
 //----------------------------------------------------------------------
 // SwapHeader
-//  Do little endian to big endian conversion on the bytes in the
+//  Do little endian to big endian conversion on the bytes in the 
 //  object file header, in case the file was generated on a little
 //  endian machine, and we're now running on a big endian machine.
 //----------------------------------------------------------------------
 
-static void
+static void 
 SwapHeader (NoffHeader *noffH)
 {
-    noffH->noffMagic = WordToHost(noffH->noffMagic);
-    noffH->code.size = WordToHost(noffH->code.size);
-    noffH->code.virtualAddr = WordToHost(noffH->code.virtualAddr);
-    noffH->code.inFileAddr = WordToHost(noffH->code.inFileAddr);
-    noffH->initData.size = WordToHost(noffH->initData.size);
-    noffH->initData.virtualAddr = WordToHost(noffH->initData.virtualAddr);
-    noffH->initData.inFileAddr = WordToHost(noffH->initData.inFileAddr);
-    noffH->uninitData.size = WordToHost(noffH->uninitData.size);
-    noffH->uninitData.virtualAddr = WordToHost(noffH->uninitData.virtualAddr);
-    noffH->uninitData.inFileAddr = WordToHost(noffH->uninitData.inFileAddr);
+  noffH->noffMagic = WordToHost(noffH->noffMagic);
+  noffH->code.size = WordToHost(noffH->code.size);
+  noffH->code.virtualAddr = WordToHost(noffH->code.virtualAddr);
+  noffH->code.inFileAddr = WordToHost(noffH->code.inFileAddr);
+  noffH->initData.size = WordToHost(noffH->initData.size);
+  noffH->initData.virtualAddr = WordToHost(noffH->initData.virtualAddr);
+  noffH->initData.inFileAddr = WordToHost(noffH->initData.inFileAddr);
+  noffH->uninitData.size = WordToHost(noffH->uninitData.size);
+  noffH->uninitData.virtualAddr = WordToHost(noffH->uninitData.virtualAddr);
+  noffH->uninitData.inFileAddr = WordToHost(noffH->uninitData.inFileAddr);
 }
 
 //----------------------------------------------------------------------
@@ -54,79 +54,143 @@ SwapHeader (NoffHeader *noffH)
 //
 //  Assumes that the object code file is in NOFF format.
 //
-//  First, set up the translation from program memory to physical
+//  First, set up the translation from program memory to physical 
 //  memory.  For now, this is really simple (1:1), since we are
 //  only uniprogramming, and we have a single unsegmented page table
 //
 //  "executable" is the file containing the object code to load into memory
 //----------------------------------------------------------------------
 
-AddrSpace::AddrSpace(OpenFile *executable)
+AddrSpace::AddrSpace(OpenFile *executable, PCB* pcb)
 {
     NoffHeader noffH;
     unsigned int i, size;
+    int read;
 
     executable->ReadAt((char *)&noffH, sizeof(noffH), 0);
-    if ((noffH.noffMagic != NOFFMAGIC) &&
-            (WordToHost(noffH.noffMagic) == NOFFMAGIC))
-        SwapHeader(&noffH);
+    if ((noffH.noffMagic != NOFFMAGIC) && 
+    (WordToHost(noffH.noffMagic) == NOFFMAGIC))
+      SwapHeader(&noffH);
     ASSERT(noffH.noffMagic == NOFFMAGIC);
 
-// how big is address space?
-    size = noffH.code.size + noffH.initData.size + noffH.uninitData.size
-           + UserStackSize; // we need to increase the size
-    // to leave room for the stack
+    // how big is address space?
+    size = noffH.code.size + noffH.initData.size + noffH.uninitData.size 
+      + UserStackSize;  // we need to increase the size
+            // to leave room for the stack
     numPages = divRoundUp(size, PageSize);
     size = numPages * PageSize;
 
     ASSERT(numPages <= NumPhysPages);   // check we're not trying
-    // to run anything too big --
-    // at least until we have
-    // virtual memory
+            // to run anything too big --
+            // at least until we have
+            // virtual memory
 
-    DEBUG('a', "Initializing address space, num pages %d, size %d\n",
+    memoryManager->lock->Acquire();
+    DEBUG('a', "Initializing address space, num pages %d, size %d\n", 
           numPages, size);
-// first, set up the translation
-    pageTable = new TranslationEntry[numPages];
-    for (i = 0; i < numPages; i++) {
-        pageTable[i].virtualPage = i; // for now, virtual page # = phys page #
-        pageTable[i].physicalPage = i;
-        pageTable[i].valid = TRUE;
-        pageTable[i].use = FALSE;
-        pageTable[i].dirty = FALSE;
-        pageTable[i].readOnly = FALSE;  // if the code segment was entirely on
-        // a separate page, we could set its
-        // pages to be read-only
+
+    if (numPages <= memoryManager->getNumFreePages()) {
+
+        this->pcb = pcb;
+
+        // Set up the page table
+        pageTable = new TranslationEntry[numPages];
+        for (i = 0; i < numPages; i++) {
+            pageTable[i].virtualPage = i;
+            pageTable[i].physicalPage = memoryManager->getPage();
+            pageTable[i].valid = TRUE;
+            pageTable[i].use = FALSE;
+            pageTable[i].dirty = FALSE;
+            pageTable[i].readOnly = FALSE;
+        }
+        memoryManager->lock->Release();
+        
+        machineLock->Acquire();
+        // Zero out the physical pages allocated to this process.
+        for (i = 0; i < numPages; i++) {
+            int physAddr = pageTable[i].physicalPage * PageSize;
+            bzero(&(machine->mainMemory[physAddr]), PageSize);
+        }
+        machineLock->Release();
+
+        fprintf(stderr, "Loaded Program: %d code | %d data | %d bss\n",
+            noffH.code.size, noffH.initData.size, noffH.uninitData.size);
+
+        // then, copy in the code and data segments into memory using new
+        // ReadFile functionality
+        if (noffH.code.size > 0) {
+            DEBUG('a', "Initializing code segment, at 0x%x, size %d\n", 
+                noffH.code.virtualAddr, noffH.code.size);
+            read = ReadFile(noffH.code.virtualAddr,executable,noffH.code.size,
+                noffH.code.inFileAddr);
+        }
+        if (noffH.initData.size > 0) {
+            DEBUG('a', "Initializing data segment, at 0x%x, size %d\n", 
+                noffH.initData.virtualAddr, noffH.initData.size);
+            read = ReadFile(noffH.initData.virtualAddr,executable,
+                noffH.initData.size,noffH.initData.inFileAddr);
+        }
     }
 
-// zero out the entire address space, to zero the unitialized data segment
-// and the stack segment
-    bzero(machine->mainMemory, size);
-
-// then, copy in the code and data segments into memory
-    if (noffH.code.size > 0) {
-        DEBUG('a', "Initializing code segment, at 0x%x, size %d\n",
-              noffH.code.virtualAddr, noffH.code.size);
-        executable->ReadAt(&(machine->mainMemory[noffH.code.virtualAddr]),
-                           noffH.code.size, noffH.code.inFileAddr);
+    else { // Not enough free pages to acquire.
+        memoryManager->lock->Release();
+        pageTable = NULL;
+        pcb = new PCB(-1,-1);
     }
-    if (noffH.initData.size > 0) {
-        DEBUG('a', "Initializing data segment, at 0x%x, size %d\n",
-              noffH.initData.virtualAddr, noffH.initData.size);
-        executable->ReadAt(&(machine->mainMemory[noffH.initData.virtualAddr]),
-                           noffH.initData.size, noffH.initData.inFileAddr);
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::AddrSpace
+//     Copy constructor that makes an identical copy of "other" address space.
+//----------------------------------------------------------------------
+
+AddrSpace::AddrSpace(const AddrSpace* other, PCB* pcb) {
+    
+    ASSERT(other->numPages <= NumPhysPages);
+
+    // Copy all page table entries over, create associated PCB
+    numPages = other->numPages;
+    memoryManager->lock->Acquire();
+    DEBUG('a', "Initializing address space with num pages: %d.\n", numPages);
+
+    if (numPages <= memoryManager->getNumFreePages()) {
+
+        this->pcb = pcb;
+        pageTable = new TranslationEntry[numPages];
+	//Allocate physical pages for each page in the new process under pcb
+	//Implement me
+        memoryManager->lock->Release();
+
+        machineLock->Acquire();
+	//Copy page content of the other process to the new address space page by page
+        //Implement me
+        
+        machineLock->Release();
+    }
+    else {// Cannot fit into the current available memory
+        memoryManager->lock->Release();
+        pageTable = NULL;
+        pcb = new PCB(-1,-1);
     }
 
 }
 
 //----------------------------------------------------------------------
 // AddrSpace::~AddrSpace
-//  Dealloate an address space.  Nothing for now!
+//  Deallocate an address space.  Nothing for now!
 //----------------------------------------------------------------------
 
 AddrSpace::~AddrSpace()
 {
-    delete [] pageTable;
+    if (isValid()) {
+        memoryManager->lock->Acquire();
+        for (int i = 0; i < numPages; i++) { // free the pages
+            memoryManager->clearPage(pageTable[i].physicalPage);
+        }
+        memoryManager->lock->Release();
+        delete [] pageTable;
+        delete pcb;
+    }
 }
 
 //----------------------------------------------------------------------
@@ -144,20 +208,22 @@ AddrSpace::InitRegisters()
 {
     int i;
 
+    machineLock->Acquire();
     for (i = 0; i < NumTotalRegs; i++)
-        machine->WriteRegister(i, 0);
+  machine->WriteRegister(i, 0);
 
     // Initial program counter -- must be location of "Start"
-    machine->WriteRegister(PCReg, 0);
+    machine->WriteRegister(PCReg, 0); 
 
     // Need to also tell MIPS where next instruction is, because
     // of branch delay possibility
     machine->WriteRegister(NextPCReg, 4);
 
-    // Set the stack register to the end of the address space, where we
-    // allocated the stack; but subtract off a bit, to make sure we don't
-    // accidentally reference off the end!
+   // Set the stack register to the end of the address space, where we
+   // allocated the stack; but subtract off a bit, to make sure we don't
+   // accidentally reference off the end!
     machine->WriteRegister(StackReg, numPages * PageSize - 16);
+    machineLock->Release();
     DEBUG('a', "Initializing stack register to %d\n", numPages * PageSize - 16);
 }
 
@@ -169,9 +235,8 @@ AddrSpace::InitRegisters()
 //  For now, nothing!
 //----------------------------------------------------------------------
 
-void AddrSpace::SaveState()
-{
-}
+void AddrSpace::SaveState() 
+{}
 
 //----------------------------------------------------------------------
 // AddrSpace::RestoreState
@@ -181,7 +246,7 @@ void AddrSpace::SaveState()
 //      For now, tell the machine where to find the page table.
 //----------------------------------------------------------------------
 
-void AddrSpace::RestoreState()
+void AddrSpace::RestoreState() 
 {
     machine->pageTable = pageTable;
     machine->pageTableSize = numPages;
@@ -189,22 +254,92 @@ void AddrSpace::RestoreState()
 
 //----------------------------------------------------------------------
 // AddrSpace::Translate
-//  Convert a virtual address provided by a process into a phsyical
-//  memory address.
+//
+//     Converts a virtual address to a physical address.
+//
+//     Returns -1 for an error in translation, or else the corresponding
+//     physical page number.
 //----------------------------------------------------------------------
 
-int AddrSpace::Translate(int virtualAddr)
-{
-    int frame, physicalAddr;
-    int page = virtualAddr / PageSize;
-    int offset = virtualAddr % PageSize;
+int AddrSpace::Translate(int virtualAddress) {
 
-    // Make sure this is a valid virtual address
-    if (virtualAddr < 0 || page > numPages)
-        return -1;
+    int pageTableIndex = virtualAddress / PageSize;
+    int offset = virtualAddress % PageSize;
+    int physicalAddress = 0;
 
-    frame = pageTable[page].physicalPage;
-    physicalAddr = frame * PageSize + offset;
+    if (virtualAddress < 0 || pageTableIndex > numPages) {
+        physicalAddress = -1;
+    } else {
+        physicalAddress = 
+            pageTable[pageTableIndex].physicalPage * PageSize + offset;
+    }
 
-    return physicalAddr;
+    return physicalAddress;
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::ReadFile
+//     
+//     Loads the code and data segments into the translated memory.
+//----------------------------------------------------------------------
+
+int AddrSpace::ReadFile(int virtAddr, OpenFile* file, int size, int fileAddr) {
+
+    int numBytesRead = 0;
+    int numBytesLeft = size;
+    int numBytesToReadThisLoop = 0;
+    int physAddr = 0;
+    int currVirtAddr = virtAddr;
+    int bytesFound = 0;
+
+    while (numBytesRead < size) {
+
+        if (numBytesLeft > PageSize) {
+            numBytesToReadThisLoop = PageSize;
+        } else {
+            numBytesToReadThisLoop = numBytesLeft;
+        }
+        diskBufferLock->Acquire();
+        bytesFound = file->ReadAt(diskBuffer, numBytesToReadThisLoop, fileAddr);
+        diskBufferLock->Release();
+
+        physAddr = Translate(currVirtAddr);
+
+        if (physAddr != -1) { // zero the dest bytes, then copy over
+            
+            machineLock->Acquire();
+            bzero(&(machine->mainMemory)[physAddr], numBytesToReadThisLoop);
+            bcopy(&(diskBuffer[0]), &((machine->mainMemory)[physAddr]),
+                numBytesToReadThisLoop);
+            machineLock->Release();
+
+            currVirtAddr += numBytesToReadThisLoop;
+            numBytesLeft -= numBytesToReadThisLoop;
+
+            numBytesRead += bytesFound;
+            fileAddr += bytesFound;
+        } 
+        else {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::getPCB
+//     Returns the associated PCB for this address space.
+//----------------------------------------------------------------------
+
+PCB* AddrSpace::getPCB() {
+    return this->pcb;
+}
+
+//----------------------------------------------------------------------
+// AddrSpace::isValid
+//     Checks that we were able to allocate a new address space successfully.
+//----------------------------------------------------------------------
+
+bool AddrSpace::isValid() {
+    return (pcb != NULL);
 }
--- Project 2/code/userprog/processmanager.cc
+++ Project 2/code/userprog/processmanager.cc
@@ -1,48 +1,134 @@
-// processmanager.cc
-//
+/*
+ * ProcessManager implementation
+ *
+ * This class keeps track of the current PCBs and manages their creation and
+ * deletion.
+*/
 
 #include "processmanager.h"
+#include "utility.h" // definition of NULL
+#include "system.h" // definition of processManagerLock
 
-//----------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // ProcessManager::ProcessManager
-//  Create a new process manager to manager MAX_PROCESSES processes.
-//----------------------------------------------------------------------
+//     Constructor that initializes the PCB list, condition list, lock list, 
+//     and address space list to MAX_PROCESSES size.
+//-----------------------------------------------------------------------------
+ProcessManager::ProcessManager() : processesBitMap(MAX_PROCESSES) {
 
-ProcessManager::ProcessManager()
-{
-    pids = new BitMap(MAX_PROCESSES);
+    pcbList = new PCB*[MAX_PROCESSES];
+    conditionList = new Condition*[MAX_PROCESSES];
+    lockList = new Lock*[MAX_PROCESSES];
+    addrSpaceList = new AddrSpace*[MAX_PROCESSES];
+
+    numAvailPIDs = MAX_PROCESSES;
+    for (int i = 0; i < MAX_PROCESSES; i++) {
+        lockList[i] = NULL; // Used for joining
+        conditionList[i] = NULL; // Used for joining
+        pcbList[i] = NULL;
+        addrSpaceList[i] = NULL;
+    }
 }
 
-//----------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // ProcessManager::~ProcessManager
-//  Deallocate a process manager.
-//----------------------------------------------------------------------
+//     Destructor that frees up the memory allocated by the constructor.
+//-----------------------------------------------------------------------------
 
-ProcessManager::~ProcessManager()
-{
-    delete pids;
+ProcessManager::~ProcessManager() {
+    delete [] pcbList;
+    delete [] conditionList;
+    delete [] lockList;
+    delete [] addrSpaceList;
 }
 
-//----------------------------------------------------------------------
-// ProcessManager::allocPid
-//  Allocate an unused PID to be used by a process.
-//
-//  For now do nothing.
-//----------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// ProcessManager::getPID
+//     Returns the first free PID found.
+//-----------------------------------------------------------------------------
 
-int ProcessManager::allocPid()
-{
-    return 0;
+int ProcessManager::getPID() {
+
+    int newPID = processesBitMap.Find();
+    processesWaitingOnPID[newPID] = 0;
+    processesWaitingOnPID[newPID]++;
+    return newPID;
 }
 
-//----------------------------------------------------------------------
-// ProcessManager::freePid
-//  Deallocate a PID that is in use so that it can be allocated again by
-//  another process.
-//
-//  For now do nothing.
-//----------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// ProcessManager::clearPID
+//     Decrements the number of processes waiting on this guy before its PID
+//     can be reused, and frees the respective PID for re-use if possible.
+//-----------------------------------------------------------------------------
 
-void ProcessManager::freePid(int pid)
-{
+void ProcessManager::clearPID(int pid) {
+
+    processesWaitingOnPID[pid]--;
+    if (processesWaitingOnPID[pid] == 0) {
+        processesBitMap.Clear(pid);
+    }
+}
+
+//-----------------------------------------------------------------------------
+// ProcessManager::addProcess
+//     Add a new process to the list.
+//-----------------------------------------------------------------------------
+
+void ProcessManager::addProcess(PCB* pcb, int pid) {
+    pcbList[pid] = pcb;
+}
+
+//-----------------------------------------------------------------------------
+// ProcessManager::join
+//     Allows process A to wait on another process B in order to perform a join
+//     system call.
+//-----------------------------------------------------------------------------
+
+void ProcessManager::join(int pid) {
+
+    Lock* lockForOtherProcess = lockList[pid];
+    if (lockForOtherProcess == NULL) {
+        lockForOtherProcess = new Lock("");
+        lockList[pid] = lockForOtherProcess;
+    }
+
+    Condition* conditionForOtherProcess = conditionList[pid];
+    if (conditionForOtherProcess == NULL) {
+        conditionForOtherProcess = new Condition("");
+        conditionList[pid] = conditionForOtherProcess;
+    }
+   // Increment  processesWaitingOnPID[pid].
+   // Conditional waiting on when it becomes 0. When it bcomes 0, recycle pid.
+   // Implement me. 
+
+}
+
+//-----------------------------------------------------------------------------
+// ProcessManager::broadcast
+//     Lets everyone know that the process has changed status so that other 
+//     processes can act accordingly if they are waiting.
+//-----------------------------------------------------------------------------
+
+void ProcessManager::broadcast(int pid) {
+
+    Lock* lock = lockList[pid];
+    Condition* condition = conditionList[pid];
+    pcbStatuses[pid] = pcbList[pid]->status;
+
+    if (condition != NULL) { // something is waiting on this process
+	// Wake up others 
+	// Implement me
+    }
+}
+
+//-----------------------------------------------------------------------------
+// ProcessManager::getStatus
+//     Returns the status of a given process
+//-----------------------------------------------------------------------------
+
+int ProcessManager::getStatus(int pid) {
+    if (processesBitMap.Test(pid) == 0) {
+        return -1; // process finished
+    }
+    return pcbStatuses[pid];
 }
